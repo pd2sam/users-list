b83a5b3d95787ab1c79156c366a62f77796df154
pd2sam <pdsam@bk.ru>
2026-04-08 20:04:44 +0300

    настройка ошибок

--- src/pages/userPage.tsx
+++ src/pages/userPage.tsx
@@ -10,16 +10,27 @@ import { UserCard } from "@/entities/user/ui/UserCard";
 export function UserPage() {
     const [users, setUsers] = useState<IUser[]>([]);
     const [isLoading, setIsLoading] = useState(true);
-    
+    const [error, setError] = useState<string | null>(null);
+
+
 
     useEffect(() => {
         const fetchUsers = async () => {
             try {
                 const response = await fetch('https://6981cd53c9a606f5d448166e.mockapi.io/users');
+                if (!response.ok) {
+                    throw new Error('Не удалось загрузить данные');
+                }
                 const data = await response.json();
                 setUsers(data);
-            } catch (error) {
-                console.error('Ошибка при загрузке:', error);
+            } catch (e) {
+                if (e instanceof Error) {
+                    setError(e.message);
+                } else {
+                    setError('Что-то пошло не так');
+                }
+
+
             } finally {
                 setIsLoading(false);
             }
@@ -28,19 +39,22 @@ export function UserPage() {
     }, [])
     if (isLoading) {
         return <div>
-            <Spin/>
+            <Spin />
         </div>
     };
+    if (error) {
+        return <p>Ошибка: {error}</p>
+    }
     return (
         <div>
             {users.map((user) => (
-                <UserCard 
+                <UserCard
                     key={user.id}
                     id={user.id}
                     name={user.name}
                     createdAt={user.createdAt}
                     avatar={user.avatar}
-                    />
+                />
             ))}
         </div>
     )
